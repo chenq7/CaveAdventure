66fd3fc3d1f3f91c81ea319ed27950f896986b69
Qixiang <qixiang100@gmail.com>
2020-08-18 22:28:17 -0400

    added beam class

---
+++ src/scenes/Beam.js
@@ -0,0 +1,28 @@
+import Phaser from "phaser";
+
+class Beam extends Phaser.GameObjects.Sprite {
+  constructor(scene){
+
+    let x = scene.player.x + 16;
+    let y = scene.player.y;
+
+    super(scene, x, y, "beam");
+
+    scene.add.existing(this);
+
+    this.setScale(1.5);
+    this.play("beam_animation");
+    scene.physics.world.enableBody(this);
+    this.body.velocity.x = 450;
+
+    scene.projectiles.add(this);
+  }
+
+  update() {
+    if (this.x > 900) {
+      this.destroy();
+    }
+  }
+}
+
+export default Beam;
