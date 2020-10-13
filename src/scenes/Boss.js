862ba48b0b2680a1fff8e83d9da38fc2af1851fc
Qixiang <qixiang100@gmail.com>
2020-10-13 04:12:26 -0400

    finished coding boss logic

---
+++ src/scenes/Boss.js
@@ -0,0 +1,44 @@
+import Phaser from "phaser";
+import BossMissle from "./BossMissle";
+
+class Boss extends Phaser.GameObjects.Sprite {
+  constructor(scene, xCoord, yCoord) {
+
+    let x = xCoord;
+    let y = yCoord;
+
+    super(scene, x, y, "boss");
+
+    scene.add.existing(this);
+
+    this.setScale(0.75);
+    this.play("boss_animation");
+    scene.physics.world.enableBody(this);
+
+    scene.enemies.add(this);
+
+    this.timer = 1;
+    this.hp = 2000;
+    this.damage = -171;
+    this.value = 5000;
+  }
+
+  update() {
+    if (this.x < 0) {
+      this.destroy();
+    }
+
+    if (this.timer % 8 === 0){
+      new BossMissle(this.scene, 600, 300, "missle1", this.randomInt(0, 160) - 80);
+    }
+
+    this.timer++;
+  }
+
+  randomInt(min, max){
+    return Math.random() * (max - min + 1) + min; 
+  }
+
+}
+
+export default Boss;
